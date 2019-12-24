7acd1b482ff1ed79a63b09f1bb1c07bc96c5bbdb
Florian Patruck <florian.patruck@hdnet.de>
2019-12-24 13:48:36 +0100

    Added a lucky number page

---
+++ src/Controller/LuckyNumberController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyNumberController extends AbstractController
+{
+    /**
+     * @Route("/{yourNumber}", name="index", methods={"GET"})
+     */
+    public function index($yourNumber = -1)
+    {
+        if($yourNumber === -1)
+            $yourNumber = random_int(0, 100);
+
+        return $this->render('lucky_number.html.twig', ['number' => $yourNumber]);
+    }
+}
